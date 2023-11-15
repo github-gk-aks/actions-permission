20afe3237a3657ddd3d57c4bf56f17441088b167
gauravkhanna3007 <gaurav3007@gmail.com>
2023-11-16 00:24:39 +0530

    Preparing repo for test

---
+++ script/python-script.py
@@ -0,0 +1,14 @@
+import sys
+import pandas as pd
+
+repository_name = sys.argv[1]  # Repository name passed as a command line argument
+
+# Read Excel file
+excel_data = pd.read_excel('data/repo_data.xlsx', header=None)
+repository_names = excel_data.iloc[:, 0].tolist()
+
+# Check if the current repository name is in the list
+allow_access = repository_name in repository_names
+
+# Output 'true' or 'false' to be used in the subsequent step
+print(f"::set-output name=allowAccess::{str(allow_access).lower()}")
